e03f4139692e782920fc91051ed2a42b7792aa00
Thomas Kerin <afk11@users.noreply.github.com>
2015-08-26 02:08:39 +0100

    Add Certificate / CertificateInfo

---
+++ src/Crypto/Certificates/Certificate.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Mdanter\Ecc\Crypto\Certificates;
+
+
+use Mdanter\Ecc\Crypto\Signature\SignatureInterface;
+
+class Certificate
+{
+    /**
+     * @var CertificateInfo
+     */
+    private $info;
+
+    /**
+     * @var string
+     */
+    private $sigAlg;
+
+    /**
+     * @var SignatureInterface
+     */
+    private $signature;
+
+    /**
+     * @param CertificateInfo $info
+     * @param string $sigAlg
+     * @param SignatureInterface $signature
+     */
+    public function __construct(
+        CertificateInfo $info,
+        $sigAlg,
+        SignatureInterface $signature
+    ) {
+        $this->info = $info;
+        $this->sigAlg = $sigAlg;
+        $this->signature = $signature;
+    }
+
+    /**
+     * @return CertificateInfo
+     */
+    public function getInfo()
+    {
+        return $this->info;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSigAlgorithm()
+    {
+        return $this->sigAlg;
+    }
+
+    /**
+     * @return SignatureInterface
+     */
+    public function getSignature()
+    {
+        return $this->signature;
+    }
+}
