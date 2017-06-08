4f5d930ea1fd59aab89c6b8a1d65c78245949725
Thomas Kerin <thomas@blockcorp.com>
2017-06-08 22:49:27 +0200

    Signer: correctly pad signatures up to nearest multiple of 4 bytes (test case had sha256 with 62 bytes)

--- src/Crypto/Signature/Signer.php
+++ src/Crypto/Signature/Signer.php
@@ -35,7 +35,8 @@ class Signer
     public function truncateHash(GeneratorPoint $G, \GMP $hash)
     {
         $dec = $this->adapter->toString($hash);
-        $hexSize = BinaryString::length($this->adapter->decHex($dec));
+        $hexSize = (int) ceil(BinaryString::length($this->adapter->decHex($dec)) / 4) * 4;
+
         $hashBits = $this->adapter->baseConvert($dec, 10, 2);
         if (BinaryString::length($hashBits) < $hexSize * 4) {
             $hashBits = str_pad($hashBits, $hexSize * 4, '0', STR_PAD_LEFT);
