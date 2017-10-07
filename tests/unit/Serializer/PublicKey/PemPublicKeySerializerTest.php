f9b56fc3f629c55f52ebebb36ab13ede77346d82
Thomas Kerin <thomas@blockcorp.com>
2017-10-07 16:38:59 +0200

    Replace openssl-{priv,pub}.{key,pem} fixtures
    I don't think there was ever a plan for consistency
    amongst the test fixtures, but they are a handy way
    to make the examples deterministic..
    
    The *.pem keys were for secp256k1
     - the public key belonged to the private key
    
    The *.der keys were for secp256r1/nistp256
     - the private and public keys didn't match
    
    This replaces those with two keys:
    `openssl-secp256r1`, and `openssl-secp256r1.1`
    in public and private format, in both encodings.

--- tests/unit/Serializer/PublicKey/PemPublicKeySerializerTest.php
+++ tests/unit/Serializer/PublicKey/PemPublicKeySerializerTest.php
@@ -13,7 +13,7 @@ class PemPublicKeySerializerTest extends AbstractTestCase
 {
     public function testReadsPem()
     {
-        $der = file_get_contents(__DIR__ . "/../../../data/openssl-pub.pem");
+        $der = file_get_contents(__DIR__ . "/../../../data/openssl-secp256r1.1.pub.pem");
         $adapter = EccFactory::getAdapter();
         $derSerializer = new DerPublicKeySerializer($adapter);
         $pemSerializer = new PemPublicKeySerializer($derSerializer);
