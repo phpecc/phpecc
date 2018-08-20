aaed8fceecf4da9c430fd478020824e5a0188e1e
Thomas Kerin <thomas@blockcorp.com>
2018-08-20 15:27:18 +0200

    rewrite Signature/Der/Parser to not use template parser

--- src/Serializer/Signature/Der/Parser.php
+++ src/Serializer/Signature/Der/Parser.php
@@ -3,8 +3,10 @@ declare(strict_types=1);
 
 namespace Mdanter\Ecc\Serializer\Signature\Der;
 
-use FG\ASN1\Exception\ParserException;
+use FG\ASN1\ASNObject;
 use FG\ASN1\Identifier;
+use FG\ASN1\Universal\Integer;
+use FG\ASN1\Universal\Sequence;
 use Mdanter\Ecc\Crypto\Signature\Signature;
 use Mdanter\Ecc\Crypto\Signature\SignatureInterface;
 use Mdanter\Ecc\Exception\InvalidSignatureException;
@@ -18,25 +20,29 @@ class Parser
      */
     public function parse(string $binary): SignatureInterface
     {
-        $template = [
-            Identifier::SEQUENCE => [
-                Identifier::INTEGER,
-                Identifier::INTEGER,
-            ],
-        ];
+        $offsetIndex = 0;
+        $asnObject = ASNObject::fromBinary($binary, $offsetIndex);
 
-        $parser = new \FG\ASN1\TemplateParser();
-        try {
-            $sequence = $parser->parseBinary($binary, $template);
-        } catch (ParserException $e) {
-            throw new InvalidSignatureException("Invalid ASN.1 for signature", 0, $e);
-        } catch (\Exception $e) {
-            throw new InvalidSignatureException("Invalid DER for signature", 0, $e);
+        if ($offsetIndex != strlen($binary)) {
+            throw new InvalidSignatureException('Invalid data.');
+        }
+
+        // Set inherits from Sequence, so use getType!
+        if ($asnObject->getType() !== Identifier::SEQUENCE) {
+            throw new InvalidSignatureException('Invalid tag for sequence.');
+        }
+
+        if ($asnObject->getNumberofChildren() !== 2) {
+            throw new InvalidSignatureException('Invalid data.');
+        }
+
+        if (!($asnObject[0] instanceof Integer && $asnObject[1] instanceof Integer)) {
+            throw new InvalidSignatureException('Invalid data.');
         }
 
         return new Signature(
-            gmp_init($sequence[0]->getContent(), 10),
-            gmp_init($sequence[1]->getContent(), 10)
+            gmp_init($asnObject[0]->getContent(), 10),
+            gmp_init($asnObject[1]->getContent(), 10)
         );
     }
 }
